f4f4e663fce0b9ba3042ccdc213358252d85ebdd
Martin <martynf8888@gmail.com>
2021-09-08 16:48:46 +0200

    Added Home and About pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'#which controller it sends it to # action/method inside of it
+  get 'about', to: 'pages#about'
 end
